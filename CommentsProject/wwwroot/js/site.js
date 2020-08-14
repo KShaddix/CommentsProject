f9827cc3665f07dc575af7f7108093475a472b61
Ilya Tretyakov <kshaddix1413@gmail.com>
2020-08-14 17:47:01 +0300

    Add Articles and Article pages

--- CommentsProject/wwwroot/js/site.js
+++ CommentsProject/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+  $('*[id^=answer_').bind('click', function () {
+    var numberId = $(this).attr('id').substr(7);
+    var articleId = $('#articleId').attr('data-article-id');
 
-// Write your JavaScript code.
+    var commentElement = $('#comment_' + numberId);
+    if (commentElement.children('#answer-form_' + numberId).length > 0)
+      commentElement.children('#answer-form_' + numberId)[0].remove();
+    else
+      commentElement.append(`
+        <div id="answer-form_${numberId}" class="border-top mt-3 pt-2">
+            <form action="/Article/${articleId}" method="post">
+                <div>
+                  <label class="article__comment-label">ID пользователя:</label>
+                  <input type="number" name="userId" />
+                </div>
+                <div>
+                    <label class="article__comment-label">Комментарий:</label>
+                    <textarea name="text" rows="5" cols="30"></textarea>
+                </div>
+                <input type="hidden" name="articleId" value="${articleId}" />
+                <input type="hidden" name="parentId" value="${numberId}" />
+                <div>
+                  <input type="submit" value="Отправить" />
+                </div>
+            </form>
+        </div>
+      `);
+    }
+  )
+});
